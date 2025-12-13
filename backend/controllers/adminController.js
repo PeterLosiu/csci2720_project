77d2ed9c106d9d387d92f5fc9328541c0ffa7b23
Peter <peterlosiufai@gmail.com>
2025-12-13 18:31:07 +0800

    add backend/controllers framework

---
+++ backend/controllers/adminController.js
@@ -0,0 +1,7 @@
+/**
+ * 1. Receive requests from admin users
+ * 2. Perform admin-specific operations such as user management and content moderation
+ * 3. Return appropriate responses to the client
+ * 4. Capture and deal with errors
+ * 5. Use model functions to interact with the database
+ */
---
+++ backend/controllers/locationController.js
@@ -0,0 +1,18 @@
+
+class locationController {
+    static async getLocations(req, res) {
+        /**
+         * 1. Receive requests to get location data
+         * 2. Use model functions to fetch location data from the database
+         * 3. Return the location data to the client
+         * 4. Capture and deal with errors
+         */
+    }
+
+    static async getAllLocations(req, res) {}
+
+    static async createLocation(req, res) {}
+
+    static async getLocationDistance(req, res) {}
+
+}
