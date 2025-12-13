df305840730309b058794367c9a933e636cb1cd2
Peter <peterlosiufai@gmail.com>
2025-12-13 11:49:40 +0800

    added backend/models/user.js

---
+++ backend/models/user.js
@@ -0,0 +1,18 @@
+/**
+ * Defines the User model using Mongoose Schema.
+ * 
+ * Functionality:
+ * - Defines the structure of user documents in the MongoDB collection.
+ * - Data validation for user fields, including:
+ *  - fields' types
+ *  - required fields
+ *  - unique constraints
+ *  - Default values for certain fields.
+ * 
+ * Fields:
+ * - username: String, required, unique
+ * - password: String, required
+ */
+
+
+
