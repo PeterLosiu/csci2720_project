d5c907f8fa8b59578fbaf35c086d445aa301903b
Makelith <csdsn.lv1@gmail.com>
2025-12-15 23:16:27 +0800

    update userController.js

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,14 +1,51 @@
-const UserModel = require('../models/userModel');
-const {isExist, validatePassword} = require('../middleware/userAuth.js');
+const UserModel = require('../models/User.js');
+const jwt = require('jsonwebtoken');
+const {isDuplicate, validatePassword} = require('../middleware/userAuth.js');
 
 
 class userController {
     // Getter methods (public)
+    static async getTokenInfo(req, res) {
+        let token;
+        // Expect header: Authorization: Bearer <token>
+        // get token from header
+        if (  
+          req.headers.authorization && 
+            req.headers.authorization.startsWith('Bearer ')
+        ) {
+          token = req.headers.authorization.split(' ')[1];
+        }
+        if (!token) {
+          return res.status(401).json({ message: 'Not authorized, no token provided' });
+        }
+        return token;
+    }
+    
+    static async getInfo(req, res) {
+        // get token from request
+        let token = await userController.getTokenInfo(req, res);
+        try {
+            // token has to be verified
+            const decoded = jwt.verify(
+              token,
+              process.env.JWT_SECRET || 'dev_jwt_secret' // use env var in production
+            );
+            const user = await UserModel.findById(decoded.id).select('-password'); // exclude password
+            if (!user) {
+                return res.status(404).json({ message: 'User not found' });
+            }
+            res.status(200).json(user);
+        } catch (err) {
+            console.error('Get user info error:', err);
+            res.status(500).json({ message: 'Server error while fetching user info' });
+        }   
+    }
+
     static async getInfoById(req, res) {
         /**
          * Fetch user information by user ID
          */
-        const userId = req.params.id;
+        const userId = req.params.userId;
         try {
             const user = await UserModel.findById(userId).select('-password');
             if (!user) {
@@ -19,6 +56,25 @@ class userController {
             console.error('Get user info error:', err);
             res.status(500).json({ message: 'Server error while fetching user info' });
         }
+    }
+    static async getFavoriteLocations(req, res) {
+        // get token infor from request
+        let token = await userController.getTokenInfo(req, res);
+        try {
+            // token has to be verified
+            const decoded = jwt.verify(
+              token,
+              process.env.JWT_SECRET || 'dev_jwt_secret' // use env var in production
+            );
+            const user = await UserModel.findById(decoded.id).populate('favoriteLocations'); // exclude password
+            if (!user) {
+                return res.status(404).json({ message: 'User not found' });
+            }
+            res.status(200).json(user.favoriteLocations);
+        } catch (err) {
+            console.error('Get user info error:', err);
+            res.status(500).json({ message: 'Server error while fetching user info' });
+        }   
     }
     static async getFavoriteLocationsById(req, res) {
         const userId = req.params.userId;
@@ -33,7 +89,6 @@ class userController {
             res.status(500).json({ message: 'Server error while fetching user favorites' });
         }
     }
-
     // CRUD Users (admin)
     // Create
     static async createUser(req, res) {
@@ -77,9 +132,21 @@ class userController {
         const { userId } = req.params;
         const updates = req.body;
         try {
+            // check if updates are provided
+            if(!updates){
+                return res.status(400).json({message: 'Request body is required'});
+            }
             // validate updates
-            isExist(updates.username, res);
-            validatePassword(updates.password, res);
+            const duplicate = await isDuplicate(updates.username, res);
+            if(!duplicate.success){
+                return res.status(400).json({ message: duplicate.message });
+            }
+            if(updates.password){
+                const validPassword = await validatePassword(updates.password);
+                if(!validPassword){
+                    return res.status(400).json({ message: validPassword.message });
+                }
+            }
             // perform update
             const updatedUser = await UserModel.findByIdAndUpdate(userId, updates, { new: true });
             if (!updatedUser) {
@@ -87,6 +154,7 @@ class userController {
             }
             res.status(200).json({ message: 'User updated successfully', updatedUser });
         } catch (error) {
+            console.log(error)
             res.status(500).json({ message: 'Error updating user', error });
         }
     }
