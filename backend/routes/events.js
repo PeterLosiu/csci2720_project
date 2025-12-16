452111a97874d5aa5dd94d3ff971a1ae32cf3ed4
Peter <peterlosiufai@gmail.com>
2025-12-16 09:44:41 +0800

    add routers to routes/events.js

--- backend/routes/events.js
+++ backend/routes/events.js
@@ -10,5 +10,17 @@
 const express = require('express');
 const router = express.Router();
 const EventModel = require('../models/Event.js');
+const eventController = require('../controllers/eventController.js')
+const {protect, adminOnly} = require('../middleware/auth.js')
+
+// Public routes
+router.get('/',eventController.getAllEvents)
+router.get('/:title',eventController.getEventByTitle)
+router.get('/:id',eventController.getEventById)
+
+// Admin routes
+router.post('/', protect, adminOnly, eventController.createEvent)
+router.put('/:id', protect, adminOnly, eventController.updateEvent)
+router.delete('/:id',protect,adminOnly, eventController.deleteEvent)
 
 module.exports = router;
