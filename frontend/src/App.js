f91b8eca1f60de9684557eb0e8d70329c9f258ad
Peter <peterlosiufai@gmail.com>
2025-12-18 17:55:03 +0800

    added App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,40 +4,40 @@ import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import { useParams, useLocation } from 'react-router-dom';
 
 // Importing web pages from /pages directory 
-// import Home from './pages/HomePage';
+import Home from './pages/HomePage';
 // import LocationList from './pages/LocationList';
 // import Map from './pages/Map';
 // import FavouriteList from './pages/FavouriteList';
 
 import EventList from './pages/EventList';
 // import NoMatch from './pages/NoMatch';
-// import LoginPage from './pages/LoginPage';
+import LoginPage from './pages/LoginPage';
 // import SingleLocation from './pages/SingleLocation';
 
 import Navbar from './pages/Navbar';
 
 class App extends React.Component {
   render() {
-    return(<Navbar />);
-    // return (
-    //   <BrowserRouter>
+    // return(<Navbar />);
+    return (
+      <BrowserRouter>
         
-    //     <Navbar />
+        <Navbar />
 
-    //     <hr />
+        <hr />
 
-    //     <Routes>
-    //         <Route path="/" element={<Home />} />
-    //         <Route path="/login" element={<LoginPage />} />
-    //         <Route path="/LocationList" element={<LocationList />} />
-    //         <Route path="/SingleLocation" element={<SingleLocation />} />
-    //         <Route path="/EventList" element={<EventList />} />
-    //         <Route path="/Map" element={<Map />} />
-    //         <Route path="/FavouriteList" element={<FavouriteList />} />
-    //         <Route path="*" element={<NoMatch />} />
-    //     </Routes>
-    //   </BrowserRouter>
-    // );
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<LoginPage />} />
+            {/* <Route path="/LocationList" element={<LocationList />} /> */}
+            {/* <Route path="/SingleLocation" element={<SingleLocation />} /> */}
+            {/* <Route path="/EventList" element={<EventList />} /> */}
+            {/* <Route path="/Map" element={<Map />} /> */}
+            {/* <Route path="/FavouriteList" element={<FavouriteList />} /> */}
+            {/* <Route path="*" element={<NoMatch />} /> */}
+        </Routes>
+      </BrowserRouter>
+    );
   }
 }
 
