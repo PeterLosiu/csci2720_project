85f749ea9dcbe0d219649bffbdec2bb48bc9fec6
Peter <peterlosiufai@gmail.com>
2025-12-18 16:20:30 +0800

    added index.html, package.json, Navbar.js

---
+++ frontend/src/pages/Navbar.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default Navbar = () =>{
+    return(
+        <nav class="navbar">
+            {/*左侧Logo与菜单*/}
+            <div class="navbar-left">
+                Location & Event App
+                <ul class="nav-menu">
+                    <li class="nav-item"><Link to="/">Home</Link></li>
+                    <li class="nav-item"><Link to="/LocationList">Location List</Link></li>
+                    <li class="nav-item"><Link to="/Map">Map</Link></li>
+                    <li class="nav-item"><Link to="/FavouriteList">Favourite List</Link></li>
+                    {/* <!-- 管理员专属菜单（登录后根据权限显示） --> */}
+                    {/* <li class="nav-item admin-menu" style="display: none;">
+                        User Manager
+                    </li>
+                    <li class="nav-item admin-menu" style="display: none;">
+                        Event Manager
+                    </li> */}
+                </ul>
+            </div>
+
+            {/* <!-- 右侧：主题切换+用户名+退出登录 --> */}
+            <div class="navbar-right">
+                {/* <!-- 额外功能：深色/浅色主题切换 --> */}
+                {/* <button class="theme-toggle" id="themeToggle">
+                    <i class="uil uil-moon"></i>
+                </button>
+                <div class="user-info">
+                    <span class="username" id="currentUsername"></span>
+                    <button class="logout-btn" id="logoutBtn">Sign Out</button>
+                </div> */}
+            </div>
+        </nav>
+    )
+}
