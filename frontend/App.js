799ab7f2008995bb974fd7505f8d0e1b8eb417ba
Peter <peterlosiufai@gmail.com>
2025-12-18 16:16:15 +0800

    added App.js

--- frontend/App.js
+++ frontend/App.js
@@ -1,31 +1,27 @@
+import ReactDOM from 'react-dom/client';
 import React from 'react';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { useParams, useLocation } from 'react-router-dom';
 
 // Importing web pages from /pages directory 
-import Home from './components/Home';
-import LocationList from './components/LocationList';
-import EventList from './components/EventList';
-import Map from './components/Map';
-import FavouriteList from './components/FavouriteList';
-import NoMatch from './components/NoMatch';
-import LoginPage from './components/LoginPage';
-import SingleLocation from './components/SingleLocation';
+import Home from './pages/HomePage';
+import LocationList from './pages/LocationList';
+import Map from './pages/Map';
+import FavouriteList from './pages/FavouriteList';
+
+import EventList from './pages/EventList';
+import NoMatch from './pages/NoMatch';
+import LoginPage from './pages/LoginPage';
+import SingleLocation from './pages/SingleLocation';
+
+import Navbar from './pages/Navbar';
 
 class App extends React.Component {
   render() {
     return (
       <BrowserRouter>
-        <div>
-          <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/login">Login</Link></li>
-            <li><Link to="/LocationList">Images</Link></li>
-            <li><Link to="/SingleLocation">Images</Link></li>
-            <li><Link to="/EventList">Slideshow</Link></li>
-            <li><Link to="/Map">Slideshow</Link></li>
-            <li><Link to="/FavouriteList">Slideshow</Link></li>
-            
-          </ul>
-        </div>
+        
+        <Navbar />
 
         <hr />
 
